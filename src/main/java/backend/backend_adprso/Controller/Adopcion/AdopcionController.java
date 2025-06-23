27a827179161bfd1cb844624d7deaed5759f57c1
abraham <abraham2004hf@gmail.com>
2025-06-22 22:29:29 -0500

    Cambios

--- src/main/java/backend/backend_adprso/Controller/Adopcion/AdopcionController.java
+++ src/main/java/backend/backend_adprso/Controller/Adopcion/AdopcionController.java
@@ -3,15 +3,12 @@ package backend.backend_adprso.Controller.Adopcion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import backend.backend_adprso.Controller.Response.ApiResponse;
 import backend.backend_adprso.Entity.Adopcion.AdopcionEntity;
 import backend.backend_adprso.Entity.Mascota.MascotaEntity;
